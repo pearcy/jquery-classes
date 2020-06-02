3a4648bf5bef3be31b2a66cc3136ef2d422e17eb
Joseph Pearce <joseph.pearce05@gmail.com>
2020-06-02 14:11:33 -0700

    change background color on button click, change p tag on click,
    Co-authored-by: Taylor Somers tsomers@yandex.com

--- js/scripts.js
+++ js/scripts.js
@@ -14,4 +14,24 @@ $(document).ready(function() {
     $("body").addClass("red-background");
   });
 
+  $("button#white").click(function() {
+    $("body").removeClass();
+    $("body").addClass("white-background");
+  });
+
+  $(".text1").click(function() {
+    $(".text1").toggle();
+    $(".text2").toggle();
+  });
+
+  $(".text2").click(function() {
+    $(".text1").toggle();
+    $(".text2").toggle();
+  });
+
+  // $(".text1").click(function() {
+  //   $("p").removeClass('red');
+  //   $("p").addClass('green');
+  // });
+
 });
